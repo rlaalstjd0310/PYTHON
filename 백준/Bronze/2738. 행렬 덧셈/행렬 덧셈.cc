657cb96843e881f755584a16aec7ae425aaac393
rlaalstjd0310 <minsung74310@gmail.com>
2025-09-24 18:12:04 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -9,21 +9,21 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 	vector <vector<int>> v1(n, vector<int>(m));
-	for (int i = 0; i < v1.size(); ++i) {
-		for (int j = 0; j < v1[i].size(); ++j) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
 			int a;
 			cin >> v1[i][j];
-		}	
+		}
 	}
 	vector <vector<int>> v2(n, vector<int>(m));
-	for (int i = 0; i < v2.size(); ++i) {
-		for (int j = 0; j < v2[i].size(); ++j) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
 			int a;
 			cin >> v2[i][j];
 		}
 	}
-	for (int i = 0; i < v1.size(); ++i) {
-		for (int j = 0; j < v1[i].size(); ++j) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
 			cout << v1[i][j] + v2[i][j] << " ";
 		}
 		cout << "\n";
