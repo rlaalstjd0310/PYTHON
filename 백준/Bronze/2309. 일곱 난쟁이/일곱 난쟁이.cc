022d758fc2398d3715fbf01ef9cd4a4cff5873af
rlaalstjd0310 <minsung74310@gmail.com>
2025-11-11 14:02:35 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -15,7 +15,7 @@ int main() {
 	}
 	bool found = false;
 	for (int i = 0; i < v1.size() && !found; ++i) {
-		for (int j = i+1; j < v1.size() && !found; ++j) {
+		for (int j = i+1; j < v1.size(); ++j) {
 			if (v1[i]+v1[j]==sum-100) {
 				v1.erase(v1.begin() + j);
 				v1.erase(v1.begin() + i);
