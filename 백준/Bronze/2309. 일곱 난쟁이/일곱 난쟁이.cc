d93dbaa765a1a79f6d0d1cdd8904bca29c7394e5
rlaalstjd0310 <minsung74310@gmail.com>
2025-11-11 13:59:42 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> v1;
+int sum = 0;
+int main() {
+	for(int i=0;i<9;++i){
+		int n;
+		cin >> n;
+		v1.push_back(n);
+	}
+	for (int i = 0; i < v1.size(); ++i) {
+		sum += v1[i];
+	}
+	bool found = false;
+	for (int i = 0; i < v1.size() && !found; ++i) {
+		for (int j = i+1; j < v1.size() && !found; ++j) {
+			if (v1[i]+v1[j]==sum-100) {
+				v1.erase(v1.begin() + j);
+				v1.erase(v1.begin() + i);
+				found = true;
+			}
+		}
+	}
+	sort(v1.begin(), v1.end());
+	for (int i = 0; i < v1.size(); ++i) {
+		cout << v1[i] << endl;
+	}
+	return 0;
+}
